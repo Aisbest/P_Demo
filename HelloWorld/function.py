8b436123ad2fc23992568a87b0b9d4413fbda9c6
kirin <suyz8728@gmail.com>
2023-12-30 17:35:05 +0800

    second commit

---
+++ HelloWorld/function.py
@@ -0,0 +1,23 @@
+# define a function
+# def greet():
+#     print("hi,there")
+#     print("welcome aboard")
+
+
+# greet()
+
+# different between paramater and argument?
+# A parameter is the variable listed inside the parentheses in the function definition.
+# An argument is the value that are sent to the function when it is called.
+
+# parameter
+def greet(first_name, last_name):
+    print(f"hi,{first_name} {last_name}")
+    print("welcome aboard")
+
+
+# argument
+greet("kirin", "sue")
+
+#  1- perform a task
+#  2- return a value
